7e055868a04a53a46bbddab86929aa739c42ca8e
prathamesh <luffygithub@gmail.com>
2025-03-20 09:20:24 +0530

    test

--- Java Fundamentals/addition.java
+++ Java Fundamentals/addition.java
@@ -1,9 +1,9 @@
 class Main {
 
-    // create a method
+    // simple java program
     public int addNumbers(int a, int b) {
         int sum = a + b;
-        // return value
+
         return sum;
     }
 
@@ -11,10 +11,7 @@ class Main {
 
         int a = 25;
         int b = 15;
-
-        // create an object of Main
         Main obj = new Main();
-        // calling method
         int result = obj.addNumbers(a, b);
         System.out.println("Sum is: " + result);
     }
