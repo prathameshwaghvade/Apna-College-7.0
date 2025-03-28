add16e4a3ca88df8cf82458ee9648544ec1d5c2d
prathamesh <luffygithub@gmail.com>
2025-03-28 11:49:43 +0530

    Loops and some practice questions

---
+++ 4. Loops/SquarePattern.java
@@ -0,0 +1,12 @@
+
+public class SquarePattern {
+    public static void main(String[] args) {
+        
+    
+    for(int i =0;i<4;i++){
+        System.out.println("****");
+
+    }
+}
+}
+
---
+++ 4. Loops/whileLoop.java
@@ -0,0 +1,13 @@
+
+public class whileLoop {
+    public static void main(String[] args) {
+
+        int count =1;
+        while(count<=10){
+           
+            System.out.println(count);
+            count= count+1;
+        }
+        
+    }
+}
